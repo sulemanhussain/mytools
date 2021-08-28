23c16eb61d3d73255d88d0cc9d745a299af6226d
sulemanhussain <mohdsulemanhussain@gmail.com>
2021-08-28 17:42:03 +0530

    added functionality to tictoctoe

--- js/script.js
+++ js/script.js
@@ -103,3 +103,76 @@ function displayTotal(total, inputValue) {
 function toggleCalculator(){
     $("#chkCalculatorType").val();
 }
+
+
+/* Tick Toc Toe game */
+
+var currentTurn = "X";
+var currentTurnFlag = true; // if 1 it will print X else O;
+
+var winningProbablity = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+var gameStat = ['', '', '', '', '', '', '', '', ''];
+
+var _boardCell = document.getElementsByClassName("tictactoe-board-cell");
+Array.from(_boardCell).forEach(function(element) {
+    element.addEventListener('click', fillCell);
+ });
+
+ function fillCell(){
+    var _currentCell = this;
+    var currentIndex = _currentCell.getAttribute("data-index");
+    if(currentTurnFlag) {
+        this.innerText = "X";
+        gameStat[currentIndex] = "X";
+    }  else {
+        this.innerText = "O";
+        gameStat[currentIndex] = "O";
+    }
+    currentTurnFlag = !currentTurnFlag;
+    setInterval(function() { 
+        gameValidator();
+    }, 300);
+ }
+
+ function clearBoard() {
+    _boardCell.forEach(function(cell) {
+        cell.textContent = "";
+    });
+    gameStat = ['', '', '', '', '', '', '', '', ''];
+    currentTurnFlag = true;
+ }
+
+ function gameValidator(){
+    winningProbablity.forEach(function(data){
+        var a = gameStat[data[0]];
+        var b = gameStat[data[1]];
+        var c = gameStat[data[2]];
+        if(a != "" && b != "" && c != ""){
+            if(a == b && b == c){
+                alert("Player " + a +" won the game. ");
+                clearBoard();
+                var result = 0;
+                if(a == "X"){
+                    var X = document.getElementById("txtX");
+                    result = parseInt(X.innerText);
+                    result++;
+                     X.innerText = result.toString();   
+                } else {
+                    var O = document.getElementById("txtO");
+                    result = parseInt(O.innerText);
+                    result++;
+                    O.innerText = result.toString();
+                 }
+            }
+        }
+    });
+ }
